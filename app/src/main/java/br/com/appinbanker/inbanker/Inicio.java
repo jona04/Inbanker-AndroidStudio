b5ac0405324f5a2931307554dc39f918efd9282a
Jonatas Silva <jonatassilva@iMac-de-Jonatas.local>
2016-10-18 14:00:27 -0300

    segundo commit mac

--- app/src/main/java/br/com/appinbanker/inbanker/Inicio.java
+++ app/src/main/java/br/com/appinbanker/inbanker/Inicio.java
@@ -1,7 +1,11 @@
 package br.com.appinbanker.inbanker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_inicio);
+
+        Button btn_cadastro = (Button) findViewById(R.id.btn_cadastro);
+        btn_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Script", "Clicou em techa tela de apresentação");
+
+                //quando ele apertar em fechar apresentação voltamos para o splash para mostrar o login novamente
+                Intent it = new Intent(Inicio.this, CadastroUsuario.class);
+                startActivity(it);
+
+                finish();
+            }
+        });
     }
 }
